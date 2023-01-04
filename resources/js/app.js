4c61ba4f9b92e6211be3ed4586d52fd6048ff70d
jahirulislam176 <jahid181017@gmail.com>
2023-01-05 01:11:24 +0600

    routing_not_fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,21 @@
 import './bootstrap';
 import { createApp } from 'vue'
-// import VueRouter from 'vue-router'
-import { createRouter, createWebHistory } from 'vue-router'
+import *as VueRouter from 'vue-router'
+// import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes.js'
 // import the root component App from a single-file component.
 import App from './App.vue'
 
-const router = createRouter({
-  history: createWebHistory('/'),
-  routes
+const router = VueRouter.createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: VueRouter.createWebHashHistory(),
+  routes, // short for `routes: routes`
 })
+
+// const router = createRouter({
+//   history: createWebHistory('/'),
+//   routes
+// })
 
 const app = createApp(App)
 
@@ -17,5 +23,5 @@ app.use(router)
 
 app.mount('#app')
 
-export default router
+// export default router
 
