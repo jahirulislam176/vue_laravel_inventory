1d72ce8c76d5bbe2631328f08ff33610ba7576b5
sldl2 <sldl.dev2@gmail.com>
2023-02-19 17:02:12 +0600

    nothing

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,6 +1,9 @@
 import Login from './components/auth/Login.vue'
+
 import Register from './components/auth/Register.vue'
+
 import Forgot from './components/auth/Forgot.vue'
+
 import Logout from "./components/auth/Logout.vue"
 
 import Home from './components/Home.vue'
